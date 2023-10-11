e31df788f8e94bacbf62998dbbcc4b0b4b50008f
devShahzaibAhmed <shahzaib.ahmed4362@gmail.com>
2023-10-11 05:21:53 +0500

    updated

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import "./Header.css";
-import logo from "../assets/q.svg";
-import {BiSearchAlt} from 'react-icons/bi';
+import logo from "../assets/logo.png";
+import callIcon from "../assets/call icon.svg";
+import {BiSearchAlt, BiUser, BiHeart} from 'react-icons/bi';
+import {BsCart2} from 'react-icons/bs';
 
 
 const Header=()=>{
@@ -11,10 +13,31 @@ const Header=()=>{
                 <img src={logo} alt="" />
                 <div className='search-cont'>
                     <BiSearchAlt className='icon'/>
-                    <input type="text" placeholder='Search Products'/>
 
+                    <input type="text" placeholder='Search Products'/>
                 </div>
             </div>
+
+           <div className='contact-social-container'>
+                    <div className='contact'>
+                        <img src={callIcon} alt="" />
+                    </div>
+                        <p>Call Us <br/> <span>0311-1571185</span></p>
+            <div>
+                    <div> 
+                         <BiUser/>
+                    </div>
+
+                    <div>
+                         <BiHeart/>
+                    </div>
+                    
+                    <div>
+                        <BsCart2/>
+                    </div>
+                </div>
+           </div>
+
         </div>
     );
 };
